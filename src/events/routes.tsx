dccf3cd90195b6297595b1dbc18f87e4372e5c7e
hazqeelknight <hazqeel.knight@gmail.com>
2025-09-10 01:02:51 +0500

    Events Module Frontend Implementation

--- src/events/routes.tsx
+++ src/events/routes.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { EventTypeForm } from './components/EventTypeForm';
+import { BookingDetails } from './components/BookingDetails';
 
 // Placeholder components - to be implemented by the events module developer
 const EventsOverview = React.lazy(() => import('./pages/EventsOverview'));
@@ -11,8 +13,11 @@ const EventsRoutes: React.FC = () => {
   return (
     <Routes>
       <Route index element={<EventsOverview />} />
+      <Route path="types/new" element={<EventTypeForm mode="create" />} />
+      <Route path="types/:id/edit" element={<EventTypeForm mode="edit" eventTypeId={window.location.pathname.split('/')[3]} />} />
       <Route path="types" element={<EventTypes />} />
       <Route path="bookings" element={<Bookings />} />
+      <Route path="bookings/:id" element={<BookingDetails bookingId={window.location.pathname.split('/')[3]} />} />
       <Route path="analytics" element={<Analytics />} />
     </Routes>
   );
