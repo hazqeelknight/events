f9400a19dfded0f67d5e31fd541131b32f8450ca
hazqeelknight <hazqeel.knight@gmail.com>
2025-09-10 00:43:11 +0500

    Deleted admin.py

--- backend/apps/events/admin.py
+++
@@ -1,5 +0,0 @@
-@@ .. @@
- from django.contrib import admin
- from django.utils.html import format_html
-+from django.utils import timezone
- from .models import EventType, Booking, Attendee, WaitlistEntry, CustomQuestion, BookingAuditLog
